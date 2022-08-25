c8a043bd7a631e87cf201f280f31defb94ba47b0
Kawa Ribeiro <kawa.rfaragao@gmail.com>
2022-08-25 14:45:32 -0300

    implementando views utilizando templates

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,19 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+
+        $fornecedores = [
+            0 =>[
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000-000'
+            ],
+            1 =>[
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000-000'
+            ],
+        ];
+        return view('app.fornecedor.index', compact($fornecedores));
     }
-}
+} 
