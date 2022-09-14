2a57e43bb145781ccb5780d08a28035cfc2ab3b4
Kawa Ribeiro <kawa.rfaragao@gmail.com>
2022-09-14 17:27:39 -0300

    usando protected no model Fornecedor

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome','site','uf','email'];
 }
